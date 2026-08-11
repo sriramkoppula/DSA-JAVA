e50bb99f0dcb5e8d16b385e7f00d0efd89d181c2
K.VenkataSriram <sriramkoppula41@gmail.com>
2026-08-11 17:12:34 +0000

    Solve LeetCode 48 Rotate Image

---
+++ JAVA/ARRAYS/48-Rotate-Image.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+
+        // Transpose
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix.length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Reverse each row
+        for (int i = 0; i < matrix.length; i++) {
+            int left = 0;
+            int right = matrix.length - 1;
+
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+    }
+}
