67084b6bc7f568ea5369b5bc73d32d6a5d6a2dee
K Venkata Sriram <sriramkoppula41@gmail.com>
2026-08-14 22:27:51 +0530

    Solve LeetCode 867 Transpose Matrix

---
+++ JAVA/ARRAYS/867-Transpose-Matrix.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int[][] transpose = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                transpose[i][j] = matrix[j][i];
+            }
+        }
+
+        return transpose;
+    }
+}
