20245fbe0ac8da26b6d599c0fa39a9c0545f52ce
K Venkata Sriram <sriramkoppula41@gmail.com>
2026-08-18 21:29:24 +0530

    Add Linked List basics and insertion

---
+++ JAVA/LINKEDLIST/Main.java
@@ -0,0 +1,74 @@
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class MyLinkedList {
+    Node head;
+    Node tail;
+
+    // Insert at end
+    public void insert(int data) {
+        Node n = new Node(data);
+
+        if (head == null) {
+            head = n;
+            tail = n;
+        } else {
+            tail.next = n;
+            tail = n;
+        }
+    }
+
+    // Insert at beginning
+    public void insertFirst(int data) {
+        Node n1 = new Node(data);
+
+        if (head == null) {
+            head = n1;
+            tail = n1;
+        } else {
+            n1.next = head;
+            head = n1;
+        }
+    }
+
+    // Print Linked List
+    public void print() {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        MyLinkedList l = new MyLinkedList();
+
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            l.insert(x);
+        }
+
+        int k = sc.nextInt();
+
+        l.insertFirst(k);
+
+        l.print();
+    }
+}
